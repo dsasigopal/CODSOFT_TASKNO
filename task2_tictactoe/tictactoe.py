7b405dc47d16422d05002f637319b2a2d70c8a68
dsasigopal <dsasigopal@gmail.com>
2026-07-06 17:24:59 +0530

    Update tictactoe.py

--- task2_tictactoe/tictactoe.py
+++ task2_tictactoe/tictactoe.py
@@ -20,3 +20,18 @@ def make_move(position, player):
 # Example: Player 'X' makes a move at position 4 (the center)
 make_move(4, 'X')
 print_board()
+
+def check_winner(player):
+    # Winning combinations (rows, columns, diagonals)
+    win_conditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], # Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], # Columns
+        [0, 4, 8], [2, 4, 6]             # Diagonals
+    ]
+    for condition in win_conditions:
+        if all(board[i] == player for i in condition):
+            return True
+    return False
+
+def check_draw():
+    return ' ' not in board
