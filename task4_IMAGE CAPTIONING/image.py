b452c1bbea07994bfd356ded2d5541a61d0330d5
dsasigopal <dsasigopal@gmail.com>
2026-07-16 18:05:27 +0530

    Update image.py

--- task4_IMAGE CAPTIONING/image.py
+++ task4_IMAGE CAPTIONING/image.py
@@ -1,19 +1,35 @@
-from tensorflow.keras.layers import Input, Dense, Embedding, LSTM
-from tensorflow.keras.layers import Dropout, add
-from tensorflow.keras.models import Model
+import torch
+import torch.nn as nn
+import torchvision.models as models
 
-image_input = Input(shape=(2048,))
-image_layer = Dropout(0.5)(image_input)
-image_layer = Dense(256, activation='relu')(image_layer)
+# 1. ENCODER: Extract features from images using ResNet
+class EncoderCNN(nn.Module):
+    def __init__(self):
+        super(EncoderCNN, self).__init__()
+        resnet = models.resnet50(pretrained=True)
+        # Remove the final classification layer
+        self.resnet = nn.Sequential(*list(resnet.children())[:-1])
+        
+    def forward(self, images):
+        features = self.resnet(images)
+        return features.reshape(features.size(0), -1)
 
-caption_input = Input(shape=(34,))
-caption_layer = Embedding(5000, 256, mask_zero=True)(caption_input)
-caption_layer = LSTM(256)(caption_layer)
+# 2. DECODER: Generate captions using an RNN/LSTM
+class DecoderRNN(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size):
+        super(DecoderRNN, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, batch_first=True)
+        self.fc = nn.Linear(hidden_size, vocab_size)
 
-decoder = add([image_layer, caption_layer])
-decoder = Dense(256, activation='relu')(decoder)
-output = Dense(5000, activation='softmax')(decoder)
+    def forward(self, features, captions):
+        embeddings = self.embedding(captions)
+        # Combine image features with word embeddings
+        x = torch.cat((features.unsqueeze(1), embeddings), dim=1)
+        hiddens, _ = self.lstm(x)
+        outputs = self.fc(hiddens)
+        return outputs
 
-model = Model([image_input, caption_input], output)
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam')
+# 3. INITIALIZATION
+encoder = EncoderCNN()
+decoder = DecoderRNN(embed_size=256, hidden_size=512, vocab_size=1000)
